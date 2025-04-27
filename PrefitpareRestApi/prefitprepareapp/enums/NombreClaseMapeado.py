c3363ecd437fbe42a6bacf4f43da38880ca73a41
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-04-27 19:36:28 +0200

    APIREST/Backend/Basica-v1: Agregar clase de tipo enum que contendrá y gestionará los valores de los nombres de las clases que nos interesa mostrar en django.

--- PrefitpareRestApi/prefitprepareapp/enums/NombreClaseMapeado.py
+++ PrefitpareRestApi/prefitprepareapp/enums/NombreClaseMapeado.py
@@ -6,14 +6,14 @@ class NombreClaseMapeado(Enum):
     INGREDIENTE = 3
     PLATO = 4
 
-def comprobarOpcionClase(self, numNombreClase):
+def comprobarOpcionClase(numNombreClase):
         return numNombreClase in NombreClaseMapeado._value2member_map_
 
-def generarMensajeMapeoObjeto(self, numNombreClase):
+def generarMensajeMapeoObjeto(numNombreClase):
         if not comprobarOpcionClase(numNombreClase):
             return (f"El valor introducido como argumento para generar un mensaje acorde a un número entero "
                     f"que se asocia al nombre de una clase es erróneo.\nLos únicos valores admitidos están"
                     f"comprendidos en el siguiente rango de valores: (1-{len(NombreClaseMapeado)})")
         else:
             valorNombreClaseEnum = NombreClaseMapeado(numNombreClase).name
-            return f"{valorNombreClaseEnum}"
+            return f"{valorNombreClaseEnum.title()}"
