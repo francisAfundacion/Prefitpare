2efa2942d268c994bf25e0fd5914c9b5820a67d5
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-04-27 19:56:06 +0200

    APIREST/Backend/Basica-v1: Registrar modelo usuario en el panel de administración de Django.

--- PrefitpareRestApi/prefitprepareapp/admin.py
+++ PrefitpareRestApi/prefitprepareapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from prefitprepareapp.models import Plato, Ingrediente, PersonaTipo, Categoria, Usuario
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Ingrediente)
+admin.site.register(PersonaTipo)
+admin.site.register(Categoria)
+admin.site.register(Plato)
+
