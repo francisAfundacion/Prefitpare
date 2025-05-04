8c8b27b45ee86ec14b446fecdfb2c1ffa7df4f64
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-05-04 22:39:58 +0200

    APIREST/Backend/Basica-v1: Renombrar Usuario => UsuarioPersonalizado.

---
+++ PrefitpareRestApi/prefitprepareapp/models/UsuarioPersonalizado.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UsuarioPersonalizado(AbstractUser):
+    ROLES = [
+        ('admin', 'Admin'),
+        ('asesor', 'Asesor'),
+        ('estandar','Estandar')
+    ]
+    telefono = models.CharField( max_length=9),
+    nombre = models.CharField( max_length=50),
+    rol = models.CharField( choices=ROLES, default='estandar'),
+    #A futuro tendremos una imagen de perfil por defecto.
+    ImagenPerfil = models.URLField( null=True, blank=True)
+
+    # Añadir related_name para evitar conflictos
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='usuario_set',  # Cambiar el nombre de la relación inversa
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='usuario_permissions_set',  # Cambiar el nombre de la relación inversa
+        blank=True
+    )
