87444efd9f8e03ae21b080150ca7968532ec7ab8
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-06-01 14:22:09 +0200

    autenticacion/APIREST/Backend/Basica-v1: Añadir señal relacionada con la creación de usuarios a la hora de iniciar la aplicación.

--- PrefitpareRestApi/prefitprepareapp/apps.py
+++ PrefitpareRestApi/prefitprepareapp/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class PrefitprepareappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prefitprepareapp'
+
+    def ready(self):
+        import prefitprepareapp.signals
